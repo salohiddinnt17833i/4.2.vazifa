7609b8f88623241f0c7f6202680bff294ce3db21
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-01-13 13:23:44 +0500

    main

---
+++ index.js
@@ -0,0 +1,124 @@
+/**
+ * 1
+ * Berilgan sonning musbat  yoki manfiyligini tekshiradigan algaoritm yoing.
+ */
+// let num = -10;
+//  if (num >= 0) {
+//     console.log(`Musbat son`);
+//  } else {
+//     console.log(`Manfiy son`);
+//  }
+
+/**
+ * 2
+ * Berilgan sonning  qaysi oraliqqa tegishli ekanini aniqlovchi algoritm
+ * A 90 - 100
+ * B 80 - 89
+ * C 70 - 79
+ * D 60 - 69
+ * F 0 - 59
+ */
+// let number = 20;
+// if (number >= 90 && number <= 100) {
+//     console.log(`A`);
+// } else if (number >= 80 && number <= 89) {
+//     console.log(`B`);
+// } else if (number >= 70 && number <= 79) {
+//     console.log(`C`);
+// }else if (number >= 60 && number <= 69) {
+//     console.log(`D`);
+// }else if (number >= 0 && number <= 59) {
+//     console.log(`F`);
+// }
+
+/**
+ * 3
+ * Berilgan  yil kabisa yili  ekanligini tekshiring kabisA YILI 4 ga
+ *  bolinadi lekin  100 ga bolinmaydi agar u 400 ga bolinmasa kabisa yili
+ */
+
+// let year = 2022;
+
+// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//     console.log(year +  ` kabisa yili`);
+// } else {
+//     console.log(year + ` kabisa yili emas`);
+// }
+
+
+
+/**
+ * 4
+ * Berilgan sonning musbat yoki manfiy yoki 0 ga teng ekanligini tekshiradigan algiritm yoing
+ */
+// let num2 = -10;
+
+// if (num2 > 0) {
+//     console.log(`Musbat son`);
+// } else if(num2 < 0){
+//     console.log(`Manfiy son`);
+// }else {
+//     console.log(` 0 ga teng son`);
+// }
+
+/**
+ * 5 
+ * 2 ta sondan kattasini aniqlovchi algoritm yozing
+ */
+// let number1 = 21;
+// let number2 = 11;
+// let max;
+
+// if (number1 > number2) {
+//     max = number1
+// } else {
+//     max = number2
+// }
+// console.log(max);
+
+
+/**
+ * 6
+ * 3 ta sondan Kichigini aniqlovchi algoritm yozing
+ */
+// let num1 = 15;
+// let num2 = 11;
+// let num3 = 12;
+// let min;
+
+// if (num1 > num2) {
+//     min = num2
+// }else{
+//     min = num1
+// }
+
+// if (min > num3) {
+//     min = num3
+// }
+// console.log(min);
+
+
+/**
+ * 7
+ * 3 ta sondan manfiylarining yigindisini aniqlovchi algoritm yozing
+ */
+//  let num1 = 15;
+//  let num2 = 11;
+//  let num3 = -12;
+//  let sum = 0;
+
+// if (num1 > 0) {
+//     sum = sum + num1
+// }
+// if (num2 > 0) {
+//     sum = sum + num2
+// }
+// if (num3 > 0) {
+//     sum = sum + num3
+// }
+// console.log(sum);
+
+
+
+
+
